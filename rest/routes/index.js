0e7efc328da886b92c1f2c3d3096e4f614fdf8a7
Faiz Ainur Rofiq <cavadevop@gmail.com>
2021-06-01 18:34:03 +0700

    add ping route

--- rest/routes/index.js
+++ rest/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/ping", function (req, res, next) {
+  res.json({
+    status: 200,
+    message: "server is running",
+    server_name: "Imblock Fabric API",
+  });
 });
 
 module.exports = router;
